5b3f063e8d6bd913991986d022fb4d341b1c3c95
subash <thapa.subash345@gmail.com>
2019-05-30 09:01:33 -0700

    initial commit

---
+++ giraffe.rb
@@ -0,0 +1,25 @@
+character_name = "John"
+character_age = "35"
+puts "Hello " + character_name
+
+age = 75
+gpa = 3.2
+isMale = true
+isTall = false
+flaws = nil
+
+phrase = " Subash "
+puts phrase.upcase()
+puts phrase.downcase()
+puts phrase.strip()
+puts phrase.length()
+puts phrase.include? "Subash"
+puts phrase[1]
+puts phrase[0,4]
+puts phrase.index("X")
+
+
+
+
+
+
